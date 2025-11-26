0b2ed8174fc40b8f068f62a13643ee445a02c4e4
franckdigital <franckalain.digital@gmail.com>
2025-11-26 08:09:49 +0000

    Fix dependency of 0099_add_courrier_statut

--- core/migrations/0099_add_courrier_statut.py
+++ core/migrations/0099_add_courrier_statut.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0098_auto_20241125_2300'),  # Ajustez selon votre dernière migration
+        ('core', '0043_courrierrappel_courrierstatut_courriernotification'),  # Ajustez selon votre dernière migration
     ]
 
     operations = [
